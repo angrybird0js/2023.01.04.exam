4d3a58dd28d9dd578dce17024d1c9f858ef0a9e4
angribird0js <jaehsh@naver.com>
2023-01-04 16:52:31 +0900

    업로드 외 완료

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const fs = require('fs')
 const app = express()
 const port = 3000
 
-const _path = path.join(__dirname, '/dist')
+const _path = path.join(__dirname, '/dist/')
 console.log(_path)
 app.use('/', express.static(_path)) // 
 app.use(logger('tiny'))
@@ -22,7 +22,7 @@ app.use(
 
 const storage = multer.diskStorage({
     destination: (req, res, cb) => {
-      cb(null, _path) // ./dist
+      cb(null, _path + "../download/") // ./dist/
     },
     filename: (req, res, cb) => {
       cb(null, res.originalname) // 
@@ -31,38 +31,46 @@ const storage = multer.diskStorage({
   
   let upload = multer({ storage: storage }) // 기본옵션은 dest
 
-app.post('/inquiry', upload.single('upfile'), function (req, res) {
+app.post('/inquiry', function (req, res) {
 
   let name = req.body.name
   let age = req.body.age
+  let txt = req.body.txt
+  let txtdata = name +':'+ age +':'+ txt
 
   fs.stat(_path + name + '.txt', (err, stats) => { //파일정보 구하는 함수 이용
     if (stats) {
       res.send(
         `<script>alert("${name}.txt 파일이름 중복으로 ${name}_1.txt 이름으로 저장합니다.");history.go(-1)</script>`
       )
-      fs.writeFile(_path + name + '_1.txt', age, (e) => {
+      fs.writeFile(_path + name + '_1.txt', txtdata, (e) => {
         if (e) throw e
       })
     } else {
-      fs.writeFile(_path + name + '.txt', age, (e) => {
+      fs.writeFile(_path + name + '.txt', txtdata, (e) => {
         
         if (e) throw e
         res.send(
-          `<script>alert("${name}.txt 파일에 저장");location.replace('index.html');</script>`
+          `<script>alert("${name}.txt 파일에 저장됨");location.replace('index.html');</script>`
         ) // history.go(-1) 선택 가능
-      })
-    }
-  })
+      }) // end fs.writefile
+    } // end if else
+  }) // end fs.stat
 
+
+})  // end app post
+
+app.post('/upload', upload.single('upfile'), (req, res) => {
+  // 파일 단독으로 전송옵션
   console.log(req.file) // app.post req 에서 파일 확인
-
-
+  res.send(
+    `<script>alert("파일 업로드 완료!");location.replace('index.html')</script>`
+  ) // history.go(-1) 
 })
 
 app.get('/test', function( req, res ) {
-    res.send(req.query.id + ',' + req.query.name)
-    // localhost:3000/test?id=77&name=nick
+    res.send( `ID : ${req.query.id} , Name : ${req.query.name} `)
+    // localhost:3000/test?id=77&name=nickname
 })
 
 app.listen(port, () => {
